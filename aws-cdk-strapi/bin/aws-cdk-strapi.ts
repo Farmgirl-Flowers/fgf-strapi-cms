7530d80b4e0a2da4ab02ff11d519a001d0ad34b9
Maarten Claes <maartencls@gmail.com>
2024-06-19 16:25:13 +0200

    CDK: Set up cloudfront

--- aws-cdk-strapi/bin/aws-cdk-strapi.ts
+++ aws-cdk-strapi/bin/aws-cdk-strapi.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { AwsCdkStrapiStack } from '../lib/aws-cdk-strapi-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { AwsCdkStrapiStack } from "../lib/aws-cdk-strapi-stack";
 
 const app = new cdk.App();
-new AwsCdkStrapiStack(app, 'AwsCdkStrapiStack', {
+new AwsCdkStrapiStack(app, "AwsCdkStrapiStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -12,6 +12,10 @@ new AwsCdkStrapiStack(app, 'AwsCdkStrapiStack', {
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: "us-east-1",
+  },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
--- aws-cdk-strapi/lib/aws-cdk-strapi-stack.ts
+++ aws-cdk-strapi/lib/aws-cdk-strapi-stack.ts
@@ -2,15 +2,49 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import { CloudFrontTarget } from "aws-cdk-lib/aws-route53-targets";
 
 export class AwsCdkStrapiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const cmsUploadsSubdomain = "cms-uploads";
+
     // The code that defines your stack goes here
 
     const bucket = new s3.Bucket(this, "bucket", {
       bucketName: "fgf-cms-uploads-prod",
+    });
+
+    const hostedZone = cdk.aws_route53.HostedZone.fromLookup(
+      this,
+      "hostedZone",
+      {
+        domainName: "farmgirlflowers.com",
+      },
+    );
+
+    const certificate =
+      cdk.aws_certificatemanager.Certificate.fromCertificateArn(
+        this,
+        "certificate",
+        "arn:aws:acm:us-east-1:536448734625:certificate/23a6488b-0539-4182-8747-27a05a440206",
+      );
+
+    const distribution = new cloudfront.Distribution(this, "cdn", {
+      comment: "CDN for Strapi CMS uploads",
+      defaultBehavior: { origin: new origins.S3Origin(bucket) },
+      domainNames: [`${cmsUploadsSubdomain}.farmgirlflowers.com`],
+      certificate,
+    });
+    const cdnRecord = new cdk.aws_route53.ARecord(this, "cdnRecord", {
+      zone: hostedZone,
+      recordName: cmsUploadsSubdomain,
+      target: cdk.aws_route53.RecordTarget.fromAlias(
+        new CloudFrontTarget(distribution),
+      ),
     });
 
     const group = new iam.Group(this, "group", {
