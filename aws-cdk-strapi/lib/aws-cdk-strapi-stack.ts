d1a005cd709896d12a03816b7849f3cdc52d5170
Maarten Claes <maartencls@gmail.com>
2024-06-19 15:41:44 +0200

    CDK: Define basic stack with bucket and IAM setup

--- aws-cdk-strapi/lib/aws-cdk-strapi-stack.ts
+++ aws-cdk-strapi/lib/aws-cdk-strapi-stack.ts
@@ -1,6 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class AwsCdkStrapiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,9 +9,28 @@ export class AwsCdkStrapiStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkStrapiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new s3.Bucket(this, "bucket", {
+      bucketName: "fgf-cms-uploads-prod",
+    });
+
+    const group = new iam.Group(this, "group", {
+      groupName: "strapi-admin",
+      path: "/app/",
+    });
+
+    // Users in this group have full admin access to the S3 bucket
+    group.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:*"],
+        resources: [bucket.arnForObjects("*")],
+      }),
+    );
+
+    // Create user for the admin panel backend
+    const adminUser = new iam.User(this, "adminUser", {
+      userName: "strapi-admin",
+      path: "/app/",
+    });
+    adminUser.addToGroup(group);
   }
 }
