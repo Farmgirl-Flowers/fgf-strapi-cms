950cee8618dcf0e1a30bab2ed084814c4e5ebd49
Maarten Claes <maartencls@gmail.com>
2024-06-19 15:32:50 +0200

    AWS CDK: Init new project

---
+++ aws-cdk-strapi/test/aws-cdk-strapi.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkStrapi from '../lib/aws-cdk-strapi-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-strapi-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkStrapi.AwsCdkStrapiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
